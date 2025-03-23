8e70f632bf1f67b07a5f333df58e276cfb50c484
Vigneshkalvium <vipvignesh.dr.2244@gmail.com>
2025-03-23 10:11:11 +0530

    completed task

---
+++ database.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.DB_URL, {
+            // useNewUrlParser: true,
+            // useUnifiedTopology: true,
+            // useCreateIndex: true,
+        });
+        console.log('MongoDB connected');
+    } catch (error) {
+        console.error(error.message);
+        process.exit(1);
+    }
+};
+
+module.exports = connectDB;
